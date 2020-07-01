eea053bf1110ee5521420a6d5701532feeb07f8a
Roman Zubko <skcorpoma@gmail.com>
2020-07-01 15:51:09 +0300

    Updates

---
+++ precache-manifest.7782c4b93609df6552fbabea2a34dff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0f8a15a9ec50af5278683e2addaaf7",
+    "url": "/react_redux-airport/arrivals/index.html"
+  },
+  {
+    "revision": "e47b6ce0c1f882715979",
+    "url": "/react_redux-airport/arrivals/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "f4529995080d225b3ae9",
+    "url": "/react_redux-airport/arrivals/static/css/main.a5623686.chunk.css"
+  },
+  {
+    "revision": "e47b6ce0c1f882715979",
+    "url": "/react_redux-airport/arrivals/static/js/2.23cdf33c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react_redux-airport/arrivals/static/js/2.23cdf33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4529995080d225b3ae9",
+    "url": "/react_redux-airport/arrivals/static/js/main.feeb2493.chunk.js"
+  },
+  {
+    "revision": "0dc7ff3a58012c2a2c5a",
+    "url": "/react_redux-airport/arrivals/static/js/runtime-main.3d0d0461.js"
+  }
+]);
